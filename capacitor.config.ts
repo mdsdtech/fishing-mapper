e13081757f6cc974319cffae75e8832907e4aada
MDSDTech <mdsdstech98@gmail.com>
2025-07-17 08:41:34 +0800

    fix: Update appId in configuration for correct project identification

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.testing.fishing.mapper',
   appName: 'Fishing Mapper',
   webDir: 'dist'
 };
